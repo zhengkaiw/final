a3044176c22423a2bad811d64ac4361770206571
JaneWJ <wang.jia1@husky.neu.edu>
2017-04-19 12:07:42 -0700

    Update AddVehicle.java
    done!

--- AddVehicle.java
+++ AddVehicle.java
@@ -8,22 +8,21 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/* 仍然使用上次的gridLayout，也显示不出结果了……*/
-
 @SuppressWarnings("serial")
 public class AddVehicle extends JFrame {
 
 	private Category category;
-	private JPanel panel;
-	private JLabel headline, categoryLabel, yearLabel, makeLabel, modelLabel, trimLabel, typeLabel, priceLabel;
+	private JPanel panel,headPanel,bottomPanel;
+	private JLabel headline, idLabel, webIdLabel, categoryLabel, yearLabel, makeLabel, modelLabel, trimLabel, typeLabel,
+			priceLabel, photoLabel;
 	private JComboBox<Category> categoryCB;
-	private JTextField yearText, makeText, modelText, trimText, typeText, priceText;
+	private JTextField idText, webIdText, yearText, makeText, modelText, trimText, typeText, priceText, photoText;
 	private JButton clearButton, confirmButton;
 
 	public AddVehicle() {
 		createComponents();
-		createLayout();
 		addComponents();
+		createLayout();
 		addActionListener();
 		display();
 	}
@@ -31,40 +30,48 @@ public class AddVehicle extends JFrame {
 	@SuppressWarnings("static-access")
 	public void createComponents() {
 
+		panel = new JPanel();
+		headPanel = new JPanel();
+		bottomPanel=new JPanel();
+
 		headline = new JLabel("ADD VEHICLE");
+		idLabel = new JLabel("ID");
+		webIdLabel = new JLabel("WebId");
 		categoryLabel = new JLabel("Category");
 		yearLabel = new JLabel("Year");
 		makeLabel = new JLabel("Make");
 		modelLabel = new JLabel("Model");
 		trimLabel = new JLabel("Trim");
-		typeLabel = new JLabel("Type");
+		typeLabel = new JLabel("BodyType");
 		priceLabel = new JLabel("Price");
+		photoLabel = new JLabel("Photo URL");
 
+		idText = new JTextField(5);
+		webIdText = new JTextField(5);
 		yearText = new JTextField(5);
 		makeText = new JTextField(5);
 		modelText = new JTextField(5);
 		trimText = new JTextField(5);
 		typeText = new JTextField(5);
 		priceText = new JTextField(5);
+		photoText = new JTextField(5);
 
 		categoryCB = new JComboBox<Category>(category.values());
 
-		clearButton = new JButton("Clear");
+		clearButton = new JButton(" Clear ");
 		confirmButton = new JButton("Confirm");
-	}
-
-	public void createLayout() {
-		setLayout(new BorderLayout(5, 5));
-		panel = new JPanel();
-		panel.setLayout(new GridLayout(0, 2));
-
+		
 	}
 
 	public void addComponents() {
 
-		Container con = getContentPane();
-		con.add("Center", panel);
-		panel.add(headline);
+		
+
+		headPanel.add(headline);
+		panel.add(idLabel);
+		panel.add(idText);
+		panel.add(webIdLabel);
+		panel.add(webIdText);
 		panel.add(categoryLabel);
 		panel.add(categoryCB);
 		panel.add(yearLabel);
@@ -79,10 +86,52 @@ public class AddVehicle extends JFrame {
 		panel.add(typeText);
 		panel.add(priceLabel);
 		panel.add(priceText);
-		panel.add(clearButton);
-		panel.add(confirmButton);
+		panel.add(photoLabel);
+		panel.add(photoText);
+		bottomPanel.add(clearButton);
+		bottomPanel.add(confirmButton);
 
 	}
+	
+	public void createLayout() {
+		
+		setLayout(new BorderLayout(5, 5));
+		
+		Container con = getContentPane();
+		con.add("Center", panel);
+		con.add("North", headPanel);
+		con.add("South", bottomPanel);
+		
+		GridLayout grid = new GridLayout(0,2);
+		FlowLayout flow=new FlowLayout();
+		panel.setLayout(grid);
+		panel.setBackground(Color.WHITE);
+		bottomPanel.setLayout(flow);
+		
+
+		Font f1 = new Font("Meiryo UI",Font.BOLD, 20);
+		Font f2 = new Font("Meiryo UI", Font.BOLD, 40);
+		Font f3 = new Font("Meiryo UI", Font.BOLD, 30);
+		changeFont(con,f1);
+		
+		//headPanel.setBackground(Color.ORANGE);
+		headline.setForeground(Color.RED);
+		headline.setFont(f2);
+		
+		categoryCB.setBackground(Color.WHITE);
+		
+		clearButton.setBackground(Color.RED);
+		clearButton.setForeground(Color.WHITE);
+		clearButton.setFont(f3);
+
+		confirmButton.setBackground(Color.RED);
+		confirmButton.setForeground(Color.WHITE);
+		confirmButton.setFont(f3);
+
+
+	}
+
+
 
 	public void addActionListener() {
 		clearButton.addActionListener(new ActionListener() {
@@ -96,16 +145,26 @@ public class AddVehicle extends JFrame {
 			}
 		});
 	}
+	
+	public void changeFont(Component component, Font font) {
+
+		component.setFont(font);
+		if (component instanceof Container) {
+			for (Component child : ((Container) component).getComponents()) {
+				changeFont(child, font);
+			}
+		}
+	}
 
 	public void display() {
 
-		setSize(400, 400);
+		setSize(800, 800);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	public static void main(String[] args) {
-		new AddVehicleToInventory();
+		new AddVehicle();
 	}
 
 }
